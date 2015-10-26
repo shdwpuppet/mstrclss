34aa45d947ca9ee5ccd3b03dc9199cb038e43098
shdwpuppet <shdw.puppet@gmail.com>
2015-10-26 18:25:24 -0400

    jesus fuck class based forms should die.

--- mstrclss/urls.py
+++ mstrclss/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='templates/landing.html')),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^classes/', 'classes.views.index', name='class_index'),
+    url(r'^classes/manage', 'classes.views.add_or_edit_class', name='class_manager')
 ]
 urlpatterns += (
     url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT, 'show_indexes': True}),
